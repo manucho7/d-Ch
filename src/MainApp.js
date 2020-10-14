448a72c4abde8907c22f5bf3a8a857783d9e8ca5
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-14 14:19:32 -0300

    Generando rutas publicas principales, vinculacion con bootstrap, componentes HomePage y UserDetail

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { HomePage } from './components/ui/HomePage';
+import { UserDetail } from './components/ui/UserDetail';
 
 export const MainApp = () => {
     return (
-        <div>
-            <h1>Hola mundo</h1>
-            <hr />
+        <Router>
+            <div className="container">
+                <h1>Navbar</h1>
+                <hr />
 
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={ HomePage }
+                    >
+                    </Route>
 
-        </div>
+                    <Route 
+                        path="/:id"
+                        component={ UserDetail }
+                    >
+                    </Route>
+
+                </Switch>
+            </div>
+        </Router>
     )
 }
---
+++ src/components/ui/UserDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const UserDetail = () => {
+    return (
+        <div>
+            <h1>User details</h1>
+            <hr />
+
+        </div>
+    )
+}
