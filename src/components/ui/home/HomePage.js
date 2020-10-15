27c5a60dc6b03e48947b55d871d76297d39a15ba
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-15 17:28:52 -0300

    lista de usuarios en Home, y empezando a filtrar 1 usuario en UserDetails

--- src/components/ui/home/HomePage.js
+++ src/components/ui/home/HomePage.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Navbar } from '../navbar/Navbar';
 import { Footer } from '../footer/Footer';
-import { GetUser } from '../../helpers/GetUser';
-
-
+import axios from "axios";
 
 export const HomePage = () => {
 
-    const { users } = GetUser();
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get('https://jsonfy.com/users')
+        .then(result => setData(result.data));
+    }, []);
+
+    console.log(data);
+    console.log(data[0]);
 
     return (
         <>
             <Navbar />
             <h1>esta es la Home</h1>
             <hr />
-            {
-                users
-            }
+
+            <ul>
+                {
+                    data.map(user => (
+                        <li key="user.id">
+                            {user.id}: {user.name}
+                        </li>
+                    ))
+                }
+            </ul>
 
             <Footer />
         </>
--- src/components/ui/users/UserDetail.js
+++ src/components/ui/users/UserDetail.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer } from '../footer/Footer';
 import { Navbar } from '../navbar/Navbar';
+import axios from "axios";
 
 export const UserDetail = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+          .get(`https://jsonfy.com/users`)
+          .then(result => setData(result.data));
+      }, []);
+  
+      console.log(data);
+      console.log(data[0]);
+
     return (
         <div>
             <Navbar />
             <h1>User Details</h1>
             <hr />
+
+            {
+                data.map( user => (
+                   <li>{user.name}</li> 
+                ))
+            }
+
 
             <Footer />
         </div>
